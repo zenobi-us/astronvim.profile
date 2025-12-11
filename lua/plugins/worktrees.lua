c85dbb4bfef630ac363e10dbc05a117c3077dabf
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-12-11 20:07:02 +1030

    fix: worktress via snackes

--- lua/plugins/worktrees.lua
+++ lua/plugins/worktrees.lua
@@ -7,9 +7,9 @@ return {
     require("worktrees").setup()
   end,
   keys = {
-    { "<leader>gws", function() require("telescope").extensions.worktrees.list_worktrees() end, desc = "Switch worktree" },
-    { "<leader>gwn", function() require("worktrees").create_worktree() end, desc = "New worktree" },
-    { "<leader>gwc", function() require("worktrees").create_worktree_from_branch() end, desc = "Create worktree from existing branch" },
-    { "<leader>gwr", function() require("worktrees").remove_worktree() end, desc = "Remove worktree" },
+    { "<leader>gws", function() Snacks.picker.worktrees() end, desc = "Switch worktree" },
+    { "<leader>gwn", function() Snacks.picker.worktrees_new() end, desc = "New worktree" },
+    { "<leader>gwc", function() Snacks.picker.worktrees_new(true) end, desc = "Create worktree from existing branch" },
+    { "<leader>gwr", function() Snacks.picker.worktrees_remove() end, desc = "Remove worktree" },
   },
 }
