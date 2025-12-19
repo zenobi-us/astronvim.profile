2a43a7296bc5d7ab7cbd339bbbad06dce4e5f5a7
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-12-19 13:24:13 +1030

    fix: remove duplicate movement keys

--- lua/polish.lua
+++ lua/polish.lua
@@ -30,29 +30,3 @@ else
     cache_enabled = 0,
   }
 end
-
--- Shift+Arrow keys for line/character selection in normal and insert mode
--- Normal mode: Shift+Up/Down selects text
-vim.keymap.set("n", "<S-Up>", "vk", { noremap = true, silent = true })
-vim.keymap.set("n", "<S-Down>", "vj", { noremap = true, silent = true })
-
--- Visual mode: Shift+Up/Down continues selection
-vim.keymap.set("v", "<S-Up>", "k", { noremap = true, silent = true })
-vim.keymap.set("v", "<S-Down>", "j", { noremap = true, silent = true })
-
--- Insert mode: Shift+Up/Down selects text and exits insert mode
-vim.keymap.set("i", "<S-Up>", "<Esc>vk", { noremap = true, silent = true })
-vim.keymap.set("i", "<S-Down>", "<Esc>vj", { noremap = true, silent = true })
-
--- Shift+Ctrl+Arrow keys for word selection expansion
--- Normal mode: Shift+Ctrl+Left/Right selects by word
-vim.keymap.set("n", "<S-C-Left>", "vb", { noremap = true, silent = true })
-vim.keymap.set("n", "<S-C-Right>", "vw", { noremap = true, silent = true })
-
--- Visual mode: Shift+Ctrl+Arrow continues word selection
-vim.keymap.set("v", "<S-C-Left>", "b", { noremap = true, silent = true })
-vim.keymap.set("v", "<S-C-Right>", "w", { noremap = true, silent = true })
-
--- Insert mode: Shift+Ctrl+Arrow selects by word and exits insert mode
-vim.keymap.set("i", "<S-C-Left>", "<Esc>vb", { noremap = true, silent = true })
-vim.keymap.set("i", "<S-C-Right>", "<Esc>vw", { noremap = true, silent = true })
