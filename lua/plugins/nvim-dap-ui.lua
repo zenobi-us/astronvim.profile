b75d901ec2de8f44d0b89359597bc6a524dad41d
Zeno Jiricek <airtonix@users.noreply.github.com>
2026-01-13 18:19:50 +1030

    feat(dap): improve debugger UI initialization
    - Add listeners for attach and launch DAP events to open UI automatically
    - Complements existing initialized/terminated/exited event listeners
    - Ensures dapui opens on all debugger session starts

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -15,6 +15,8 @@ return {
     dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
     dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
     dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.attach.dapui_config = function() dapui.open() end
+    dap.listeners.before.launch.dapui_config = function() dapui.open() end
 
     -- Keymaps for dapui
     local keymap = vim.keymap.set
