50360dcf905ddb555194cb20f659fc3429e271cb
Zeno Jiricek <airtonix@users.noreply.github.com>
2026-09-01 15:10:06 +0930

    fix: remove win32yank

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,31 +2,16 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Configure clipboard for WSL with win32yank or Wayland
-if vim.env.WSL_DISTRO_NAME then
-  vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-else
-  vim.g.clipboard = {
-    name = "wl-clipboard",
-    copy = {
-      ["+"] = "wl-copy",
-      ["*"] = "wl-copy",
-    },
-    paste = {
-      ["+"] = "wl-paste",
-      ["*"] = "wl-paste",
-    },
-    cache_enabled = 0,
-  }
-end
+-- Configure clipboard for WSLg Wayland
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy",
+    ["*"] = "wl-copy",
+  },
+  paste = {
+    ["+"] = "wl-paste",
+    ["*"] = "wl-paste",
+  },
+  cache_enabled = 0,
+}
