c7f4295d434d613601aaa4ca7131020ace083f85
Zeno Jiricek <airtonix@users.noreply.github.com>
2026-09-05 16:15:28 +0930

    fix(ts): attempt to address errors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
   init = function()
     require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
       local path = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
@@ -9,13 +12,16 @@ return {
       return filename:match(".*mise.*%.toml$") ~= nil
     end, { force = true, all = false })
   end,
-  opts = {
-    ensure_installed = {
+  config = function()
+    local treesitter = require "nvim-treesitter"
+    treesitter.setup()
+    treesitter.install {
       "lua",
       "vim",
       "toml",
       "bash",
       "kdl",
-    },
-  },
+      "graphql",
+    }
+  end,
 }
